892be8d30a115965f7e53922e1f0bbdb5ef849b4
Black <blackng10.5.1997@gmail.com>
2021-01-30 11:56:59 +0700

    'update

--- pages/cart.js
+++ pages/cart.js
@@ -1,11 +1,60 @@
 import Head from "next/head";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { DataContext } from "../store/GlobalState";
 import { addToCart } from "../store/Actions";
+import CartItem from "../components/CartItem";
+import Link from "next/link";
+import { getData } from "../utils/fetchData";
 
 const Cart = () => {
 	const [state, dispatch] = useContext(DataContext);
-	const { cart } = state;
+	const { cart, auth } = state;
+	const [total, setTotal] = useState(0);
+
+	useEffect(() => {
+		const getTotal = () => {
+			const res = cart.reduce((prev, item) => {
+				return prev + item.price * item.quantity;
+			}, 0);
+
+			setTotal(res);
+		};
+
+		getTotal();
+	}, [cart]);
+
+	useEffect(() => {
+		const cartLocal = JSON.parse(localStorage.getItem("__next_cart01"));
+		if (cartLocal && cartLocal.length > 0) {
+			let newArr = [];
+			const updateCart = async () => {
+				for (const item of cartLocal) {
+					const res = await getData(`product/${item._id}`);
+					const { _id, title, images, price, inStock } = res.product;
+
+					if (inStock > 0) {
+						newArr.push({
+							_id,
+							title,
+							images,
+							price,
+							inStock,
+
+							quantity:
+								item.quantity > inStock ? 1 : item.quantity,
+						});
+					}
+				}
+
+				dispatch({
+					type: "ADD_CART",
+					payload: newArr,
+				});
+			};
+
+			updateCart();
+		}
+	}, []);
 
 	if (cart.length === 0) {
 		return (
@@ -18,10 +67,51 @@ const Cart = () => {
 	}
 
 	return (
-		<div>
+		<div className="row mx-auto ">
 			<Head>
 				<title>Cart Page</title>
 			</Head>
+			<div className="col-md-8 text-secondary table-responsive my-3">
+				<h2 className="text-uppercase">Shopping Cart</h2>
+				<table className="table my-3">
+					<tbody>
+						{cart.map((item) => (
+							<CartItem
+								key={item._id}
+								item={item}
+								dispatch={dispatch}
+								cart={cart}
+							/>
+						))}
+					</tbody>
+				</table>
+			</div>
+			<div className="col-md-4 text-right text-uppercase text-secondary">
+				<form>
+					<h2>Shipping</h2>
+					<label htmlFor="adress">Adress</label>
+					<input
+						type="text"
+						name="adress"
+						id="adress"
+						className="form-control mb-2"
+					/>
+					<label htmlFor="adress">Mobile</label>
+					<input
+						type="text"
+						name="mobile"
+						id="mobile"
+						className="form-control mb-2"
+					/>
+				</form>
+
+				<h3>
+					Total: <span className="text-danger">${total}</span>
+				</h3>
+				<Link href={auth.user ? "#" : "/signin"}>
+					<a className="btn btn-dark my-2">Proceed with Payment</a>
+				</Link>
+			</div>
 		</div>
 	);
 };
